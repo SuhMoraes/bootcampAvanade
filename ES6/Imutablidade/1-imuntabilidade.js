9151588162b067a9d8eec0ccf04403cb88c1c6d9
SuhMoraes <suh.alvesmoraes@gmail.com>
2020-12-02 08:25:05 -0300

    escape

---
+++ ES6/Imutablidade/1-imuntabilidade.js
@@ -0,0 +1,8 @@
+const user= {
+  name: 'Suhailah',
+  lastName: 'Alves '
+};
+
+function getUserWithFullName(user) {
+
+}
---
+++ ES6/variaveis/1-variaveis.js
@@ -0,0 +1,5 @@
+// escopo  global
+
+// escopo de bloco
+
+//escopo de função
