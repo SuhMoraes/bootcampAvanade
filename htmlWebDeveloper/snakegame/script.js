43e465e02a1c6762fa20011e6a031b31275b61ca
Suh Moraes <suh.alvesmoraes@gmai.com>
2021-04-21 11:05:36 -0300

    Snake Game - Movement :snake:

--- htmlWebDeveloper/snakegame/script.js
+++ htmlWebDeveloper/snakegame/script.js
@@ -1,11 +1,19 @@
 let canvas = document.getElementById("snake")
 let context = canvas.getContext("2d");
+/*Tamanho da box */
 let box = 32;
+
+ /* criando a cobrinha - seu tamanho*/
 let snake = [];
 snake[0] = {
   x: 8 * box,
   y: 8 * box
 }
+
+/* Criando as direções dos movimentos*/
+let direction = "right";
+
+
 
 function criarBG(){
   context.fillStyle ="#74c69d";
@@ -17,5 +25,28 @@ function criarCobrinha(){
     context.fillRect(snake[i].x, snake[i].y, box, box)
   }
 }
-criarBG();
-criarCobrinha();
+
+/* */
+function iniciarJogo(){
+  criarBG();
+  criarCobrinha();
+
+  let snakeX = snake[0].x;
+  let snakeY = snake[0].y;
+
+  if(direction == 'right') snakeX += box;
+  if(direction == 'left') snakeX -= box;
+  if(direction == 'up') snakeY -= box;
+  if(direction == 'down') snakeY += box;
+
+  snake.pop();
+
+  let newHead = {
+    x: snakeX,
+    y: snakeY
+  }
+
+  snake.unshift(newHead);
+}
+
+let jogo = setInterval(iniciarJogo, 100);
